9f5c77e6c5d434dfa7a6212f5b06e719df812cb5
williamlin <william.lin@nextdrive.io>
2023-03-05 00:29:50 +0800

    inital repo

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import IssueOverview from "./IssueOverview";
+
+function App() {
+  return (
+    <div className="bg-[#63baff30] h-full">
+      <IssueOverview />
+    </div>
+  )
+}
+
+export default App
---
+++ src/IssueOverview.tsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+
+function IssueOverview() {
+  return (
+    <Box>
+      adsa
+    </Box>
+  )
+}
+
+export default IssueOverview
